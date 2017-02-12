2a0c9db19830a6287bdae53c42366d3ff4273888
Dennis Eichhorn <spl1nes.com@googlemail.com>
2017-02-12 21:43:10 +0100

    Bulkl path fix reverse

---
+++ Models/WorkflowStatus.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Orange Management
+ *
+ * PHP Version 7.1
+ *
+ * @category   TBD
+ * @package    TBD
+ * @author     OMS Development Team <dev@oms.com>
+ * @author     Dennis Eichhorn <d.eichhorn@oms.com>
+ * @copyright  Dennis Eichhorn
+ * @license    OMS License 1.0
+ * @version    1.0.0
+ * @link       http://orange-management.com
+ */
+namespace Modules\Workflow\Models;
+
+use phpOMS\Datatypes\Enum;
+
+/**
+ * Task status enum.
+ *
+ * @category   Workflow
+ * @package    Modules
+ * @author     OMS Development Team <dev@oms.com>
+ * @author     Dennis Eichhorn <d.eichhorn@oms.com>
+ * @license    OMS License 1.0
+ * @link       http://orange-management.com
+ * @since      1.0.0
+ */
+abstract class WorkflowStatus extends Enum
+{
+    /* public */ const OPEN = 1;
+    /* public */ const WORKING = 2;
+    /* public */ const SUSPENDED = 3;
+    /* public */ const CANCELED = 4;
+    /* public */ const DONE = 5;
+    /* public */ const CLOSED = 6;
+}
