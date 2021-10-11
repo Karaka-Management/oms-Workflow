45576a28a7a573f0b2e62ee4cb5367442f3f165f
Dennis Eichhorn <spl1nes.com@googlemail.com>
2021-10-11 22:01:05 +0200

    fix phpstan and phpcs

---
+++ Models/Components/Task.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Orange Management
+ *
+ * PHP Version 8.0
+ *
+ * @package   Modules\Workflow\Models\Components
+ * @copyright Dennis Eichhorn
+ * @license   OMS License 1.0
+ * @version   1.0.0
+ * @link      https://orange-management.org
+ */
+declare(strict_types=1);
+
+// Create/update task/task element on workflow change
