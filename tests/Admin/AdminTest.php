b9fa49a572e075e9fd3cb4d3a3b8d27d0d51a37d
Dennis Eichhorn <spl1nes.com@googlemail.com>
2021-10-21 22:19:51 +0200

    make classes final

--- tests/Admin/AdminTest.php
+++ tests/Admin/AdminTest.php
@@ -17,7 +17,7 @@ namespace Modules\Workflow\tests\Admin;
 /**
  * @internal
  */
-class AdminTest extends \PHPUnit\Framework\TestCase
+final class AdminTest extends \PHPUnit\Framework\TestCase
 {
     protected const NAME = 'Workflow';
 
