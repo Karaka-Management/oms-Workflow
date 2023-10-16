c79f057afbdeb62cd6a81f4d680669cb277cc5d6
Dennis Eichhorn <spl1nnes.com@googlemail.com>
2023-10-16 23:39:39 +0000

    Add test cases for null models

--- tests/Models/NullWorkflowInstanceAbstractTest.php
+++ tests/Models/NullWorkflowInstanceAbstractTest.php
@@ -23,7 +23,7 @@ final class NullWorkflowInstanceAbstractTest extends \PHPUnit\Framework\TestCase
 {
     /**
      * @covers Modules\Workflow\Models\NullWorkflowInstanceAbstract
-     * @group framework
+     * @group module
      */
     public function testNull() : void
     {
@@ -32,7 +32,7 @@ final class NullWorkflowInstanceAbstractTest extends \PHPUnit\Framework\TestCase
 
     /**
      * @covers Modules\Workflow\Models\NullWorkflowInstanceAbstract
-     * @group framework
+     * @group module
      */
     public function testId() : void
     {
@@ -42,7 +42,7 @@ final class NullWorkflowInstanceAbstractTest extends \PHPUnit\Framework\TestCase
 
     /**
      * @covers Modules\Workflow\Models\NullWorkflowInstanceAbstract
-     * @group framework
+     * @group module
      */
     public function testJsonSerialize() : void
     {
--- tests/Models/NullWorkflowTemplateTest.php
+++ tests/Models/NullWorkflowTemplateTest.php
@@ -23,7 +23,7 @@ final class NullWorkflowTemplateTest extends \PHPUnit\Framework\TestCase
 {
     /**
      * @covers Modules\Workflow\Models\NullWorkflowTemplate
-     * @group framework
+     * @group module
      */
     public function testNull() : void
     {
@@ -32,7 +32,7 @@ final class NullWorkflowTemplateTest extends \PHPUnit\Framework\TestCase
 
     /**
      * @covers Modules\Workflow\Models\NullWorkflowTemplate
-     * @group framework
+     * @group module
      */
     public function testId() : void
     {
@@ -42,7 +42,7 @@ final class NullWorkflowTemplateTest extends \PHPUnit\Framework\TestCase
 
     /**
      * @covers Modules\Workflow\Models\NullWorkflowTemplate
-     * @group framework
+     * @group module
      */
     public function testJsonSerialize() : void
     {
