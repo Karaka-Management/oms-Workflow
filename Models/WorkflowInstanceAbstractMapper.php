5c5c6c6b97edec6a72987acb45e624f08ad8014d
Dennis Eichhorn <spl1nnes.com@googlemail.com>
2024-04-25 15:52:55 +0000

    fix tests

--- Models/WorkflowInstanceAbstractMapper.php
+++ Models/WorkflowInstanceAbstractMapper.php
@@ -24,6 +24,9 @@ use phpOMS\DataStorage\Database\Mapper\DataMapperFactory;
  * @license OMS License 2.0
  * @link    https://jingga.app
  * @since   1.0.0
+ *
+ * @template T of WorkflowInstanceAbstract
+ * @extends DataMapperFactory<T>
  */
 final class WorkflowInstanceAbstractMapper extends DataMapperFactory
 {
